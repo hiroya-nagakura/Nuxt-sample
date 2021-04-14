8b5e9a9c63784eb2f418a41f86602460517dcc83
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-04-14 15:30:11 +0900

    [add]Todo controller追加

---
+++ backend/app/controllers/api/v1/todos_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::TodosController < ApplicationController
+  def index
+    todos = Todo.all.order(created_at: :desc)
+    render json: todos, each_serializer: TodoSerializer
+  end
+
+  def show
+    todo = Todo.find_by(id: params[:id])
+    # 二重否定だとわかりづらい気がする。何か意味があるのか
+    unless todo.nil?
+      render json: todo
+    else
+      render json: { error_message: 'Not Found'}
+    end
+  end
+
+  def create
+    todo = Todo.new(set_params)
+    if todo.save
+      render json: { success_message: '保存しました' }
+    else
+      render json: todo.errors.messages
+    end
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    if todo.update(set_params)
+      render json: { success_message: '更新しました' }
+    else
+      render json: todo.errors.messages
+    end
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy
+    render json: { success_message: '削除しました' }
+  end
+
+  private
+  def set_params
+    params.require(:todo).permit(:title, :content, :is_done, :date, :time)
+  end
+end
