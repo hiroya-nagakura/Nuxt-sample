f64865f7f3f4d59407b93ff49723a8242d32218e
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-04-15 14:55:40 +0900

    [add] #3 Topページの雛形作成

---
+++ frontend/components/TopPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='top'>
+    <div class='top-contents'>
+      <TopPageSearch />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.top{
+  background-image: url(~/assets/images/Top.jpg);
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  min-height: 70vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 2em;
+  margin-top: -100px;
+}
+.top-contents{
+  background-color: rgba(0,0,0,0.5);
+  width: 90%;
+  padding: 1em 1em;
+  border-radius: 2em;
+}
+</style>
