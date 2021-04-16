e48ff5389fc60c393eaa0d56a40d4b8009f50876
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-04-16 19:05:28 +0900

    [add] UserのAPIルーティング設定

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :tasks, only: :index
-      resources :todos
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
 end
