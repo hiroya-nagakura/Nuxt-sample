fa2afdb345e6cead31d04e6296f81a471d61026d
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-04-17 19:03:25 +0900

    [add] logoutページの追加

---
+++ frontend/pages/users/account.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-row class='justify-content-center'>
+    <b-card title='logout' class='m-5'>
+      <b-col sm="8" class="p-2">
+        <p>{{ $auth.loggedIn }}</p>
+        <p>{{ $auth.user }}</p>
+        <b-button @click="userLogout">
+          logout
+        </b-button>
+      </b-col>
+    </b-card>
+  </b-row>
+</template>
+
+<script>
+export default {
+  methods: {
+    userLogout() {
+      this.$auth.logout()
+    }
+  },
+  computed: {
+    user() {
+      return this.$auth.user;
+    }
+  },
+}
+</script>
