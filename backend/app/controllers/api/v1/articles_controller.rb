c9cf7a616c108e4e3e66674eacddc73a126be29d
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-04-18 21:57:35 +0900

    [add] article一覧ページ雛形作成

--- backend/app/controllers/api/v1/articles_controller.rb
+++ backend/app/controllers/api/v1/articles_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-    render json: @articles
+    render json: {articles: @articles}
   end
 
   def create
---
+++ frontend/pages/articles/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col cols=4
+        v-for='article in articles' :key = 'article.id'
+      >
+        <b-card
+          :title='article.title'
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+        >
+          <b-card-text>
+            {{ article.content }}
+          </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      articles: []
+    }
+  },
+  mounted() {
+    this.fetchArticles();
+  },
+  methods: {
+    async fetchArticles() {
+      const response = await this.$axios.$get('http://localhost:5000/api/v1/articles')
+      this.articles = response.articles
+    }
+  }
+}
+</script>
