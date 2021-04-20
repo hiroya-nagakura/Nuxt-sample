648210c9d82f116ee71e45c5cbdcaf6bfdcce87a
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-04-20 10:12:47 +0900

    [fix] ArticleCardのコンポーネント切り出し

---
+++ frontend/components/ArticleCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-card
+    :title='article.title'
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      {{ article.content }}
+    </b-card-text>
+
+    <b-button :href="'/articles/'+article.id" variant="primary">Go detail</b-button>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: ['article']
+}
+</script>
